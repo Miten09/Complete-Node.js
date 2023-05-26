2e272c244c4452f72bad98ea06ebb3dec52e5d2a
miten09 <mitensoni108@gmail.com>
2023-05-26 19:11:44 +0530

    Mongoose started

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-// console.log(process.env);
-//123
-//4566669999
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// console.log(process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('connection successful');
+  });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
