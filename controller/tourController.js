ea442489928cee866bf55cb870c3b46b4276e651
miten09 <mitensoni108@gmail.com>
2023-05-30 19:32:31 +0530

    Filtering Complete

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,27 +1,71 @@
 const Tour = require('./../models/tourModels');
+const APIFeatures = require('./../utils/apiFeatures');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
+exports.aliasTopTours = (req, res, next) => {
+  (req.query.limit = '5'),
+    (req.query.sort = '-ratingsAverage,price'),
+    (req.query.fields = 'name,price,ratingsAverage,summary,difficulty');
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD QUERY
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-    console.log(queryObj, req.query);
 
-    const query = Tour.find(queryObj);
+    // 1A) Filtering
+    // const queryObj = { ...req.query };
+    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // excludedFields.forEach(el => delete queryObj[el]);
 
-    // const query = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
+    // // 1B) Advanced filtering
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // // console.log(JSON.parse(queryStr));
+
+    // let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   // console.log(sortBy);
+    //   // console.log(req.query.sort);
+    //   query = query.sort(sortBy);
+    //   //sort('price ratingsAverage')
+    // } else {
+    //   query = query.sort('-createdAt');
+    // }
+
+    // 3) Field Limiting
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   // console.log(req.query.fields);
+    //   // console.log(fields);
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select('-__v');
+    // }
+    // 4) Pagination
+
+    // const page = req.query.page * 1;
+    // const limit = req.query.limit * 1;
+    // const skip = (page - 1) * limit;
+
+    // // page=3&limit=10 ,1-10 ,page 1, 11-20 , page 2, 21-30 , page3
+    // query = query.skip(skip).limit(limit);
+
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page does not exist');
+    // }
 
     //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sorting()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
+    // query.sort().select().skip().limit() aapdi original query aavi laage if I wrote in chain
 
     //SEND RESPONSE
     res.status(200).json({
@@ -110,6 +154,17 @@ exports.deleteTour = async (req, res) => {
     res.status(400).json({
       status: 'failed',
       message: 'Invalid Data sent'
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = Tour.aggregate([]);
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err
     });
   }
 };
