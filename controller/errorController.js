59e12910413d83834f7d8fa206fe1fc18a01029a
miten09 <mitensoni108@gmail.com>
2023-06-02 13:53:01 +0530

    Error Handling & Password encryption completed

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,4 +1,25 @@
-const sendErorrDev = (err, res) => {
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path} : ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value:${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid Input Data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -7,7 +28,7 @@ const sendErorrDev = (err, res) => {
   });
 };
 
-const sendErorrProd = (err, res) => {
+const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -26,8 +47,15 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'developement') {
-    sendErorrDev(err, res);
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErorrProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
---
+++ models/userModels.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please Tell us your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please Provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please Provide a Valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please Provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on SAVE & CREATE
+      // Atle aapde Jyare PassWord First Time Create & Save Karsu Tyare aa validation kaam aavse
+      //  Update password ma aa validation work nai kare
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Password Are not Same'
+    }
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually changed
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
